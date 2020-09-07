af385023ae81c4cc2b6efc3f2580314303f97dd8
harshit <krharshit553@gmail.com>
2020-09-07 13:43:10 +0530

    changes in middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,11 @@ const session=require('express-session')
 const cookieParser=require('cookie-parser')
 const bodyParser=require('body-parser')
 const passport=require('passport')
-
+const flash=require('connect-flash')
 const LocalStrategy=require('passport-local').Strategy
 const router=require('express').Router()
 
+router.use(flash())
 router.use(bodyParser.urlencoded({extended:true}))
 router.use(bodyParser.json())
 
@@ -27,6 +28,8 @@ router.use(passport.session());
 router.use((req,res,next)=>{
 
     res.locals.currentuser=req.user
+    res.locals.error=req.flash("error")
+      res.locals.success=req.flash("success")
     next()
 })
 
