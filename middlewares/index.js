9764631308cd30319f073c1371e1e1472d0b4d46
harshit <krharshit553@gmail.com>
2020-09-07 12:29:52 +0530

    middlewares added

---
+++ middlewares/index.js
@@ -0,0 +1,36 @@
+const session=require('express-session')
+const cookieParser=require('cookie-parser')
+const bodyParser=require('body-parser')
+const passport=require('passport')
+
+const LocalStrategy=require('passport-local').Strategy
+const router=require('express').Router()
+
+router.use(bodyParser.urlencoded({extended:true}))
+router.use(bodyParser.json())
+
+
+router.use(cookieParser());
+router.use(session({
+      
+    secret:'djvbfkjbvkfjbvkfbvkfvkfk',
+    resave:false,
+    saveUninitialized:false,
+}))
+
+
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+
+router.use((req,res,next)=>{
+
+    res.locals.currentuser=req.user
+    next()
+})
+
+
+  
+
+module.exports=router
